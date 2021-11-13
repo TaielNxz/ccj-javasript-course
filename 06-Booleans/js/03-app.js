b1fe0ae9670e8872e1a9d9c8787e44b992fe8494
TaielNxz <TaielNxz@hotmail.com>
2021-11-13 20:52:36 -0300

    complete section 6

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,9 @@
+const autenticado = true;
+
+if( autenticado ) {
+    console.log('Si puedes ver netflix');
+} else {
+    console.log('No, no puedes verlo');
+}
+
+console.log( autenticado ? 'si puedes ver netflix' : 'No, no puedes verlo');
