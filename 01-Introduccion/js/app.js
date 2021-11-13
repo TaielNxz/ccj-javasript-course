24a7677d1d9015ef5618c578476f4a6d36f8e335
TaielNxz <TaielNxz@hotmail.com>
2021-11-13 18:27:44 -0300

    complete section 1

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,10 @@
+const nombre = prompt('Cual es tu nombre?');
+
+// Toma el nombre y lo muestra en pantalla
+document.querySelector('.contenido').innerHTML = `${ nombre } está aprendiendo js moderno`;
+
+// Asigna el valor hacia la variable producto
+const producto = 'Monitor 24 pulgadas';
+
+console.log('Hola');
+console.log('Mundo');
