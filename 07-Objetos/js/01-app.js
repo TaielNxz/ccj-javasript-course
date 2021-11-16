aee374aa6fbb16505133d70f49a509697beb09e7
TaielNxz <TaielNxz@hotmail.com>
2021-11-15 21:28:54 -0300

    complete section 7

---
+++ 07-Objetos/js/01-app.js
@@ -0,0 +1,14 @@
+const nombre = "Monitor 20 Pulgadas";
+const precio = 300;
+const disponible = true;
+
+// Un objeto agrupa too en una sola variable...
+
+// Objetc literal
+const producto = {
+    nombre: "Monitor 20 Pulgadas",
+    precio: 300,
+    disponible: true,
+}
+
+console.log(producto);
