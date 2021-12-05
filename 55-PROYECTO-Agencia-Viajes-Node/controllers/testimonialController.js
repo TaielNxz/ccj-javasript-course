84180b39e49f772527d6e79b6477065c42594a2e
TaielNxz <TaielNxz@hotmail.com>
2021-12-05 17:29:20 -0300

    complete section 55

---
+++ 55-PROYECTO-Agencia-Viajes-Node/controllers/testimonialController.js
@@ -0,0 +1,51 @@
+import { Testimonial } from "../models/Testimoniales.js";
+
+const guardarTestimonial = async (req, res) => {
+  // Validar
+  const { nombre, email, mensaje } = req.body;
+
+  const errores = [];
+
+  if (nombre.trim() === "") {
+    errores.push({ mensaje: "El nombre esta vacio" });
+  }
+
+  if (email.trim() === "") {
+    errores.push({ mensaje: "El nombre esta vacio" });
+  }
+
+  if (mensaje.trim() === "") {
+    errores.push({ mensaje: "El mensaje esta vacio" });
+  }
+
+  if (errores.length > 0) {
+    // Consultar Testimoniales Existentes
+    const testimoniales = await Testimonial.findAll();
+
+    // Mostrar la vista con errores
+    res.render("testimoniales", {
+      pagina: "Testimoniales",
+      errores,
+      nombre,
+      email,
+      mensaje,
+      testimoniales,
+    });
+  } else {
+    // Almacenarlo en la base de datos
+
+    try {
+      await Testimonial.create({
+        nombre,
+        email,
+        mensaje,
+      });
+
+      res.redirect("/testimoniales");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+
+export { guardarTestimonial };
